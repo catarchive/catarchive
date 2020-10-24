ecea6dce67857283c58c5dbfef690288b5a4e3eb
GreerPage <greerishere@gmail.com>
2020-10-23 21:28:48 -0400

    renamed net_manager.py to model_manager.py

---
+++ client/classifier/model_manager.py
@@ -0,0 +1,28 @@
+# file to hanle network file (model/net.pth)
+
+import os 
+import requests
+
+
+class ModelManager:
+    ''' class to represent the model file '''
+    
+    def __init__(self, url='https://hosting.greerpage.com/files/greer_page/net.pth'):
+        self.path = os.path.abspath('model/net.pth')
+        self.installed = os.path.isfile(self.path)
+        self.url = url
+
+    def install(self):
+        '''download and install the pre-trained net.pth file'''
+        
+        if self.installed:
+            print('model already installed')
+            return
+
+        r = requests.get(self.url, allow_redirects=True)
+        print('downloading model...')
+        os.mkdir('model')
+        open('model/net.pth', 'wb').write(r.content)
+
+        return self.path
+
