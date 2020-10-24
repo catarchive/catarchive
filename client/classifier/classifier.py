47e2c6b044c83a5039b141695be526b0d9ebd090
GreerPage <greerishere@gmail.com>
2020-10-23 21:55:54 -0400

    added classifier.py

---
+++ client/classifier/classifier.py
@@ -0,0 +1,33 @@
+from .image_processing import ImageData
+from .net import Net
+from .model_manager import ModelManager
+from .utils import device
+
+
+
+model = ModelManager()
+
+net = Net()
+net = net.float()
+net.cuda(device)
+
+if not model.installed:
+    model.install()
+
+net.load_state_dict(model.load())
+
+def classify(img):
+    """ Runs the given image through the model and returns the results. """
+    
+    data = ImageData([[True], [img]])
+    tensor_image, label, img_path = data[0]
+    tensor_image, label = tensor_image.to(device), label.to(device)
+    
+    output = net(tensor_image.float())
+
+    _, predicted = torch.max(output, 1)
+
+    return predicted.item()
+
+
+    
