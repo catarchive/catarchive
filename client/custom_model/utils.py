76d2d662f6c7ec1a44e1e1e1fed1ea39e6aab938
GreerPage <greerishere@gmail.com>
2020-10-24 01:27:49 -0400

    made it use abs paths *eye rolls

--- client/custom_model/utils.py
+++ client/custom_model/utils.py
@@ -10,11 +10,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 def get_images():
     image_values = {}
     data = [[], []]
-
-    for x in os.listdir('cats/cats/'):
-        image_values['cats/cats/'+x] = True 
-    for x in os.listdir('cats/not_cats/'):
-        image_values['cats/not_cats/'+x] = False
+    cats, not_cats = os.path.abspath(os.path.dirname(__file__) + '/cats/cats/'), os.path.abspath(os.path.dirname(__file__) + '/cats/not_cats/')
+    for x in os.listdir(cats):
+        image_values[cats+x] = True 
+    for x in os.listdir(not_cats):
+        image_values[not_cats+x] = False
     
     for x in image_values:
         data[0].append(True) if image_values[x] else data[0].append(False)
