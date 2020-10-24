2acc76f578699efff9cf76105339d5063b929edb
GreerPage <greerishere@gmail.com>
2020-10-23 23:13:35 -0400

    temporarily moved custom classifier to custom_model

--- client/test.py
+++ client/test.py
@@ -1,5 +1,5 @@
 from classifier import classify
 import requests
 
-r = requests.get('https://greerpage.com/static/images/favicon.png', stream=True)
+r = requests.get('https://greerpage.com/static/images/img1.jpg', stream=True)
 print(classify(r, is_url=True))
